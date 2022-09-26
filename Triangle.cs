81b00160a887c034803977b1157779eaba0d5310
CyberMeerkat <apouyaud@gmail.com>
2022-09-26 10:15:38 +0200

    fixed format to improve readability

--- Triangle.cs
+++ Triangle.cs
@@ -4,6 +4,7 @@ public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3) =>
         IsTriangle(side1, side2, side3) && (side1 != side2 && side2 != side3);
+    
     public static bool IsIsosceles(double side1, double side2, double side3) =>
         IsTriangle(side1, side2, side3) && (side1 == side2 || side2 == side3 || side1 == side3);
 
@@ -11,5 +12,6 @@ public static class Triangle
         IsTriangle(side1, side2, side3) && side1 == side2 && side1 == side3;
 
     public static bool IsTriangle(double side1, double side2, double side3) =>
-        side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 && side1 > 0 && side2 > 0 && side3 > 0;
+        side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2 
+        && side1 > 0 && side2 > 0 && side3 > 0;
 }
